15ea2bd70af278126629b35a590e5133c1490ea7
Ashwarya Joshi <ajoshi@tavisca.com>
2018-09-11 14:58:21 +0530

    Implemented Caching

---
+++ CachingUsingDecoratorPattern/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CachingUsingDecoratorPattern
+{
+    class ProductService : IProductService
+    {
+        public Product GetProductById(int id)
+        {
+            IRepository productRepo = new SqlDatabase();
+            Console.WriteLine("Data Retrieved from Sql");
+            return productRepo.GetProductById(id);
+        }
+
+        public List<Product> GetProducts()
+        {
+            IRepository productRepo = new SqlDatabase();
+            return productRepo.GetAllProducts();
+        }
+    }
+}
---
+++ CachingUsingDecoratorPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CachingUsingDecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new MemoryCacheProductService();
+            List<Product> products = productService.GetProducts();
+            Console.WriteLine("Enter number of Times you want data to be retrieved");
+            int times = int.Parse(Console.ReadLine());
+            while (times>0)
+            {
+                int id = int.Parse(Console.ReadLine());
+                Product product = productService.GetProductById(id);
+                Console.WriteLine(product.Id);
+                times--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
